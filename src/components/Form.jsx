47b0004eb545f5a84bda29ba36e679e7f9b52c36
sergey-oreshkin <joknife79@yandex.ru>
2022-05-15 00:23:55 +0500

    search is working

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "./UI/input/Input";
 import Button from "./UI/button/Button";
 
-const Form = () => {
+const Form = ({fetchPosts}) => {
+
+    const [keywords, setKeywords] = useState('');
+    const [hours, setHours] = useState('12');
+
+    const submit = (e) => {
+        e.preventDefault();
+        fetchPosts(keywords, hours);
+    }
+
     return (
         <form className="form">
-            <Input type="text" placeholder="Введите ключевые слова через запятую"/><br/>
+            <Input 
+                type="text" 
+                placeholder="Введите ключевые слова через запятую" 
+                value={keywords}
+                onChange={e=>setKeywords(e.target.value)}
+            />
+            <br/>
 
             Будут показаны новости за последние
-            <Input type="number" style={{width: 70 + 'px'}} value="12"/> 
+            <Input 
+                type="number" 
+                style={{width: 70 + 'px'}} 
+                value={hours}
+                onChange={e=>setHours(e.target.value)}
+            /> 
             часов.<br/>
 
-            <Button>Поиск</Button>
+            <Button onClick = {submit}>Поиск</Button>
         </form>
     );
 }
---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = ({info}) => {
+    return (
+        <div className="info">
+            {info}
+        </div>
+    );
+}
+export default Info;
